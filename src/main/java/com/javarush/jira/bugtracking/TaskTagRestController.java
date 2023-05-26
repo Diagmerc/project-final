178411555db2f67ddc0bef8bf3062cbe877ae9c0
Diagmerc <91744556+Diagmerc@users.noreply.github.com>
2023-05-26 09:23:05 +0300

    Add validation for task to tags

--- src/main/java/com/javarush/jira/bugtracking/TaskTagRestController.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskTagRestController.java
@@ -1,14 +1,17 @@
 package com.javarush.jira.bugtracking;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
 @Slf4j
+@Validated
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = TaskTagRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -20,7 +23,7 @@ public class TaskTagRestController {
 
     @PutMapping(value = "/{id}/tags", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@PathVariable long id, @RequestBody Set<String> tags) {
+    public void update(@PathVariable long id, @RequestBody @Size(min = 2, max = 32) Set<String> tags) {
         taskService.addTag(id, tags);
     }
 }
